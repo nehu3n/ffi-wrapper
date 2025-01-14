998a81fd6a4826f941af2c54cb617c41b10711ea
nehu3n <developer.nh3@gmail.com>
2025-01-14 18:33:17 -0300

    ✨ feat: Added suffix obtaining

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,27 @@ function detectRuntime(): "deno" | "bun" | "node" {
     return "node";
   }
 }
+
+function getOS(): string {
+  const runtime = detectRuntime();
+
+  if (runtime == "node" || runtime == "bun") {
+    return process.platform;
+  } else {
+    return Deno.build.os;
+  }
+}
+
+function getSuffix(): "dll" | "dylib" | "so" {
+  const platform = getOS();
+
+  const osMapping: Record<string, "dll" | "dylib" | "so"> = {
+    win32: "dll",
+    windows: "dll",
+
+    darwin: "dylib",
+    linux: "so",
+  };
+
+  return osMapping[platform] || "so";
+}
