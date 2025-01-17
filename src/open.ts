4ff8e665ee36e5c7642ccc43c4f18679e1698038
nehu3n <developer.nh3@gmail.com>
2025-01-17 01:12:24 -0300

    ✨ feat: Added types

---
+++ src/open.ts
@@ -0,0 +1,3 @@
+import type { Symbols } from "./types.d.ts";
+
+function open(libPath: string, symbols: Symbols) {}
---
+++ src/types.d.ts
@@ -0,0 +1,32 @@
+const Types = {
+  // Integer numbers
+  Int8: 0,
+  Int16: 1,
+  Int32: 2,
+  Int64: 3,
+  // Unsigned numbers
+  UInt8: 4,
+  UInt16: 5,
+  UInt32: 6,
+  UInt64: 7,
+  // Float numbers
+  Float32: 8,
+  Float64: 9,
+  // Others
+  Boolean: 10,
+  Char: 11,
+  String: 12,
+  Function: 13,
+  Pointer: 14,
+  Buffer: 15,
+} as const;
+
+type TypeValue<T> = T[keyof T];
+type Type = TypeValue<typeof Types>;
+
+export type Symbols = {
+  [key: string]: {
+    params: Type[];
+    returns: Type;
+  };
+};
